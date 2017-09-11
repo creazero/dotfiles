cf466015136d3b356279afe83d360ff0c9a2d378
Nikita Migunov <creazerodota@gmail.com>
2017-09-11 21:16:10 +0300

    update

---
+++ install.sh
@@ -0,0 +1,11 @@
+[ ! -d "$HOME/.config" ] && mkdir ~/.config
+
+ln -s ./.Xresources ~/.Xresources
+ln -s ./.Xres ~/.Xres
+ln -s ./bspwm ~/.config/bspwm
+ln -s ./mpd ~/.config/mpd
+ln -s ./polybar ~/.config/polybar
+ln -s ./neofetch ~/.config/neofetch
+ln -s ./nvim ~/.config/nvim
+ln -s ./sxhkd ~/.config/sxhkd
+ln -s ./.zshrc ~/.zshrc
